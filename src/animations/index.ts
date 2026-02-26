fd4d563db1ec9aa312f7232ce90ef1f3ab7ea7c7
izmaelmag <caspian.seagull@gmail.com>
2026-02-26 19:58:39 +0530

    deleted animation

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -6,7 +6,6 @@ import { settings as glslDemoAnimation } from "./glsl-demo";
 import { settings as r3fDemoAnimation } from "./r3f-demo";
 import { settings as dynamicstripesdemoAnimation } from "./dynamicstripesdemo";
 import { settings as dynamicstripesdemo2Animation } from "./dynamicstripesdemo-2";
-import { settings as dynamicstripesdemo3Animation } from "./dynamicstripesdemo-3";
 
 export { settings as defaultAnimation } from "./orbital";
 export { settings as orbitalAnimation } from "./orbital";
@@ -15,7 +14,6 @@ export { settings as glslDemoAnimation } from "./glsl-demo";
 export { settings as r3fDemoAnimation } from "./r3f-demo";
 export { settings as dynamicstripesdemoAnimation } from "./dynamicstripesdemo";
 export { settings as dynamicstripesdemo2Animation } from "./dynamicstripesdemo-2";
-export { settings as dynamicstripesdemo3Animation } from "./dynamicstripesdemo-3";
 
 // All animation settings keyed by ID
 export const animationSettings: Record<string, AnimationSettings> = {
@@ -25,7 +23,6 @@ export const animationSettings: Record<string, AnimationSettings> = {
   [r3fDemoAnimation.id]: r3fDemoAnimation,
   [dynamicstripesdemoAnimation.id]: dynamicstripesdemoAnimation,
   [dynamicstripesdemo2Animation.id]: dynamicstripesdemo2Animation,
-  [dynamicstripesdemo3Animation.id]: dynamicstripesdemo3Animation,
 };
 
 export type AnimationId = keyof typeof animationSettings;
